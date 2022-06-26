6f833c5ea760b9cdc948a9b19c87d8bd3e645ac8
Tamilselvan <Tamilselvan@DESKTOP-RA20VVB>
2022-06-26 07:44:47 +0530

    Second commit

--- Gethubdemo/src/test/java/selenium/Githubdemoclass.java
+++ Gethubdemo/src/test/java/selenium/Githubdemoclass.java
@@ -5,6 +5,7 @@ public class Githubdemoclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hai today sunday");
+System.out.println("Tomorow ie monday");
 	}
 
 }
